e86c2d15708b1df5dbb5260daf70dad084677ac5
Talita Beneves <talitabeneves15@gmail.com>
2021-11-11 05:31:57 -0300

    Node.js JWT Implementation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-
+const jwt = require("jsonwebtoken");
 //Register//
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -37,10 +37,19 @@ router.post("/login", async (req, res) => {
     Originalpassword !== req.body.password && 
     res.status(401).json("Wrong credentials!");
 
-    const { password, ...others } = user._doc;
+    const accessToken = jwt.sign(
+      {
+        id: user._id, 
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC, 
+      {expiresIn: "3d"}
+    );
 
-    res.status(200).json(others);
-  } catch (err) {
+    const { password, ...others } = user._doc;
+    res.status(200).json({...others, accessToken});
+
+  }catch(err) {
     res.status(500).json(err);
   }
 });
